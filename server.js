5439111c0c5e36914ccf1c97e400a0e3a3cceb2e
piyushtsr <piyushrahangdale21@gmail.com>
2021-06-22 00:20:12 +0530

    basic crud initialization

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+app.use(express.json());
+
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -36,5 +38,5 @@ const server = app.listen(
 process.on('unhandledRejection', (err, promise) => {
   console.log(`Error: ${err.message}`.red);
   // Close server & exit process
-  server.close(() => process.exit(1));
+  // server.close(() => process.exit(1));
 });
